cfd527ce13396539b50dff98ce3a62d6832da5db
Kevin <kevin.sangurima@quinnipiac.edu>
2019-04-27 17:18:54 -0400

    Chapter 16 Demo

---
+++ app/src/main/java/edu/quinnipiac/ser210/ch16/DrinkActivity.java
@@ -0,0 +1,48 @@
+package edu.quinnipiac.ser210.ch16;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DrinkActivity extends Activity {
+    public static final String EXTRA_DRINKID = "drinkId";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+        int drinkId = (Integer)getIntent().getExtras().get(EXTRA_DRINKID);
+
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
+            Cursor cursor = db.query ("DRINK", new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"}, "_id = ?",
+                    new String[] {Integer.toString(drinkId)}, null, null,null);
+            if (cursor.moveToFirst()) {
+
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                int photoId = cursor.getInt(2);
+
+                TextView name = findViewById(R.id.name);
+                name.setText(nameText);
+
+                TextView description = findViewById(R.id.description);
+                description.setText(descriptionText);
+
+                ImageView photo = findViewById(R.id.photo);
+                photo.setImageResource(photoId);
+                photo.setContentDescription(nameText);
+            }
+        } catch(SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
